0e34f84e88ccf289a5aa3aed5f125ed9c5767d4e
Nicolas Simard <simard.nicolas.0@gmail.com>
2018-01-24 23:13:44 -0500

    Add test file for the live chat.

--- test_LiveChat.py
+++ test_LiveChat.py
@@ -1,41 +1,87 @@
 import youtube.tools as yt
 import youtube.livebroadcast as lb
-import youtube.livechat as lc
+from youtube.chat import LiveChat, Chat
+import youtube.layers as layers
 import json
+import os
+import datetime
 
-CLIENT_SECRETS_FILE = "client_secrets.json"
+# The absolute of the directory where the script lives
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+# The location of the client_secrets file
+CLIENT_SECRETS_FILE = os.path.join(BASE_DIR, "client_secrets.json")
+
+# the base name of the storage file attached to the oauth2 protocol
 STORAGE_FILE_NAME = "test"
-LIVE_CHAT_REFRESH_RATE = 5
 
-SAVING_PATH = "archive\\test.json"
+TODAY = datetime.date.today()
+
+# The directory where the chats are archived
+ARCHIVE_DIR = os.path.join(BASE_DIR, "drive-archive")
+
+# The directory where today's chat (live or mock) will be saved
+CURRENT_SAVE_DIR = os.path.join(ARCHIVE_DIR, "{}".format(TODAY))
+
+REFRESH_RATE = 5
 
 if __name__ == "__main__":
-    service = yt.get_authenticated_service(CLIENT_SECRETS_FILE, STORAGE_FILE_NAME)
-    
-    live_broadcast = lb.choose_active_live_broadcast(service)
+    # Initializing the session
+    try:
+        os.mkdir(CURRENT_SAVE_DIR)
+    except FileExistsError as e:
+        pass
+    else:
+        print(">>> Directory {} created.".format(CURRENT_SAVE_DIR))
+
+    # Just making sure that the saving method works
+    Chat().save_to_json(CURRENT_SAVE_DIR, file_name="test_save_delete_me.txt")
+
+    youtube = yt.get_authenticated_service(
+        CLIENT_SECRETS_FILE,
+        STORAGE_FILE_NAME
+    )
+
+    live_broadcast = lb.choose_active_live_broadcast(youtube)
     if live_broadcast is None:
         exit("No active live broadcasts. ")
-    print("live broadcast: {}".format(json.dumps(live_broadcast.__repr__(), indent=4)))
-    live_broadcast.refresh()
+    # print("live broadcast: {}".format(json.dumps(live_broadcast.__repr__(), indent=4)))
+    # live_broadcast.refresh()
+
+    chat = Chat()
+    # Just making sure that the saving method works
+    chat.save_to_json(
+        CURRENT_SAVE_DIR,
+        file_name="test_save_delete_me.txt"
+    )
     
-    live_chat = lc.livechat_from_livebroascast(live_broadcast) 
-    print("live chat: {}".format(json.dumps(live_chat.__repr__(), indent=4)))
-    live_chat.save_to_file(SAVING_PATH)
+    live_chat = LiveChat(
+        youtube,
+        live_broadcast.get_live_chat_id(),
+        chat,
+        REFRESH_RATE
+    )
+    # Just making sure that the saving method works
+    live_chat.save_to_json(
+        CURRENT_SAVE_DIR,
+        file_name="test_save_delete_me_too.txt"
+    )
     
-    refresher = lc.LiveChatRefresher(live_chat, LIVE_CHAT_REFRESH_RATE)
-    refresher.start()
+    question_label = layers.Question(chat, live_chat)
     
-    print("Waiting for new messages.")
-    live_chat.has_new_messages.wait()
-    print("New messages!")
-    
-    print("Waiting for the live chat to end.")
-    live_chat.is_over.wait()
-    print("Done waiting for the live chat to end.")
-    
-    live_broadcast.refresh()
-    live_chat.save_to_file(SAVING_PATH)
-    print("live chat: {}".format(json.dumps(live_chat.__repr__(), indent=4)))
-        
-    
-    
+    printer = layers.Printer(chat, question_label)
+
+    print(live_chat)
+
+    live_chat.start()
+    question_label.start()
+    printer.start()
+
+    live_chat.join()
+    question_label.join()
+    printer.join()
+
+    chat.save_to_json(CURRENT_SAVE_DIR)
+    live_chat.save_to_json(CURRENT_SAVE_DIR)
+    print("Done")
+
