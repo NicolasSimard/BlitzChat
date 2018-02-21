0771dc597b5515a6da33759f3b74aa0c8b2e4923
Nicolas Simard <simard.nicolas.0@gmail.com>
2018-02-21 14:06:36 -0500

    Moved and add new utility functions to find the livechat id.

--- youtube/livebroadcast.py
+++ youtube/livebroadcast.py
@@ -1,134 +1,60 @@
 from apiclient.errors import HttpError
-
-#TODO: When refreshing a live broadcast after is has ended, there is no more
-#      liveChatId is snippets and this causes an error...
+from .chat import LiveChat
 
 class LiveBroadcast:
-    """ A LiveChat object represents a Youtube live breadcast.
+    """ A LiveChat object represents a Youtube live broadcast. It corresponds to a Youtube liveBroadcast ressource (see https://developers.google.com/youtube/v3/live/docs/liveBroadcasts#resource)
 
     Attributes:
-        youtube_service     An authenticated youtube service.
-        id                  The live boradcast's id.
-        title               The title of the live broadcast.
+        client: An authenticated youtube service.
+        id: The live boradcast's id.
+        title: The title of the live broadcast.
+        published_at: Time at which the livebroadcast was published.
+        livechat_id: The id if the associated live chat.
 
     Methods:
-        get_live_chat_id    Get the ID of the attached live chat.
-        
-    Representation:
-    {
-        "id": str,
-        "snippet": dict
-    }
+        get_livechat: Returns the associated LiveChat object
     """
 
-    def __init__(self, youtube_service, id, **snippet):
-        """ Initialize the live broadcast from a youtube service, an id and
-        keywords arguments snippet containing the snippet info of the ress. """
+    def __init__(self, client, ressource):
+        """ Initialize the live broadcast from an authenticated youtube client and a Youtube liveBroadcast ressource.
+        """
 
-        self._snippet = snippet
-        self.id = id
-        self.youtube_service = youtube_service
+        self.client = client
+        self.ressource = ressource
+
+    @property
+    def id(self):
+        return self.ressource['id']
 
     @property
     def name(self):
         """ Return the name of the live broadcast. """
-        
+
         return self.title
-        
+
     @property
     def title(self):
         """ Return the title of the live broadcast. """
-        
-        return self._snippet.get("title", "")
-        
+
+        return self.ressource['snippet']['title']
+
     @property
     def published_at(self):
         """ Return the moment at which the live broadcast started. """
-        
-        return self._snippet.get("publishedAt", "")
 
-    def get_live_chat_id(self):
-        """ Return the id of the associated live chat. """
+        return self.ressource['snippet']['publishedAt']
 
-        return self._snippet.get("liveChatId", "")
-      
-    def refresh(self):
-        try:
-            response = self.youtube_service.liveBroadcasts().list(
-                id=self.id,
-                part="snippet"
-            ).execute()
-        except HttpError as e:
-            print("An HTTP error {} occurred while refreshing Broadcast {}:\n{}"
-                .format(e.resp.status, self.name, e.content)
-            )
-        # Update self._snippet with the key/value pairs from response_snippet,
-        # overwriting existing keys
-        response_snippet = response["items"][0]["snippet"]
-        self._snippet.update(response_snippet)
-      
+    @property
+    def livechat_id(self):
+        return self.ressource['snippet']['liveChatId']
+
+    def get_livechat(self, chat):
+        """ Return the associated LiveChat object. """
+
+        return LiveChat(self.client, self.livechat_id, chat)
+
     def __repr__(self):
-        return dict([
-            ("id", self.id),
-            ("snippet", self._snippet)
-        ])
-        
+        return self.ressource
+
     def __str__(self):
         return "Live broadcast {}.".format(self.title)
-
-
-def list_active_live_broadcasts(youtube_service):
-    """List all active live broadcasts atached to the authenticated service."""
-
-    try:
-        response = youtube_service.liveBroadcasts().list(
-            broadcastStatus="active",
-            part="id, snippet"
-        ).execute()
-    except HttpError as e:
-        print("An HTTP error {} occurred while retrieving live broadcasts:\n{}"
-            .format(e.resp.status, e.content)
-        )
-    return response["items"]
-    
-def list_live_broadcast_by_id(youtube_service, id):
-    """List all active live broadcasts attached to the authenticated service."""
-
-    try:
-        response = youtube_service.liveBroadcasts().list(
-            id=id,
-            part="id, snippet"
-        ).execute()
-    except HttpError as e:
-        print("An HTTP error {} occurred while retrieving live broadcasts:\n{}"
-            .format(e.resp.status, e.content)
-        )
-    return response["items"]
-
-def live_broadcast_from_ress(youtube_service, ress):
-    """ Return a LiveBroadcast object from a youtube_service and a liveBroadcasts
-    broadcast ressource. """
-
-    return LiveBroadcast(youtube_service, ress["id"], **ress["snippet"])
-
-def choose_active_live_broadcast(youtube_service):
-    """ Choose an active live broadcast."""
-
-    live_broadcasts = list_active_live_broadcasts(youtube_service)
-
-    if len(live_broadcasts) == 0:
-        return None
-    elif len(live_broadcasts) == 1:
-        return live_broadcast_from_ress(youtube_service, live_broadcasts[0])
-    else:
-        print("Please choose an active live broadcast:")
-        for live_broadcast in live_broadcasts:
-            print("{:2d}:  id = {}"
-                .format(live_broadcasts.index(live_broadcast), live_broadcast["id"])
-            )
-        while True:
-            try:
-                index = int(input("Your choice: "))
-                return live_broadcast_from_ress(youtube_service, live_broadcasts[index])
-            except IndexError:
-                print("Invalid index.")
