149e276d67972811697f2b5e6ac3d827e3eba620
Nicolas Simard <simard.nicolas.0@gmail.com>
2018-03-09 17:26:57 -0500

    Update combine livechat backup script.

--- combine_livechat_bkp.py
+++ combine_livechat_bkp.py
@@ -2,13 +2,24 @@ from youtube.chat import combine_live_chat_backups_in_dir
 
 import tkinter
 from tkinter.filedialog import asksaveasfilename, askdirectory
+from configparser import ConfigParser
+
+# Read the config file
+config = ConfigParser()
+config.read('config.ini')
 
 tkinter.Tk().withdraw()
 print(">>> Choose the directory containing the backup files.")
-dir = tkinter.filedialog.askdirectory(title="Choose backup directory")
+dir = tkinter.filedialog.askdirectory(
+    title="Choose backup directory",
+    initialdir=config['livechat']['backup']
+)
 
-print(">>> Choose where youtu want to save the combined ressource.")
-file_name = tkinter.filedialog.asksaveasfilename(title="Choose ressource save location")
-
+print(">>> Choose where you want to save the combined ressource.")
+file_name = tkinter.filedialog.asksaveasfilename(
+    title='Save LiveChat',
+    defaultextension='json',
+    initialdir=config['DEFAULT']['archive']
+)
 combine_live_chat_backups_in_dir(dir, file_name)
 
